a868639fc696b234c0fedeee81915dd0479ab683
Виктор Ганзенко <viktorganzenko@MacBook-Pro-Viktor.local>
2022-12-10 17:42:41 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+const galleryCardSet = galleryItems.map(createGalleryCard).join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryCardSet);
+
+function createGalleryCard({ preview, original, description }) {
+  return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const STORAGE_KEY = 'formData';
+let formData = {};
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  consoleForm(e.currentTarget);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+dataFromLocalStorage();
+function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (!data) return;
+  Object.keys(data).forEach(key => {
+    formData[key] = data[key];
+    const elData = form.querySelector(`[name="${key}"]`);
+    elData.value = data[key];
+  });
+}
+
+function consoleForm(form) {
+  const feedbackData = Object.fromEntries(new FormData(form));
+  console.log('feedbackData', feedbackData);
+}
